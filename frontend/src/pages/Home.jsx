5c1d327b2fa705e7bda7bdcb64c951c82fe73937
Priyanshu Kumar <priyanshuk6395@gmail.com>
2025-03-04 22:55:15 +0530

    server check

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Userlogin from "./Userlogin";
 const Home = () => {
   return (
     <div className="bg-cover bg-[url(/images/bg.jpg)] sm:bg-[url(/images/bg-full.jpg)] h-screen pt-8 w-full flex sm:items-center justify-between flex-col dark:bg-gray-800">
-      <img className="w-20 sm:w-25 ml-7 sm:bg-white p-1 rounded-md" src="public/images/logo.png"  alt="Logo" />
+      <img className="w-20 sm:w-25 ml-7 sm:bg-white p-1 rounded-md" src="images/logo.png"  alt="Logo" />
       <div className="bg-white px-9 pb-8 py-5 sm:rounded sm:mb-4 sm:w-1/2 sm:px-5 sm:py-4 sm:mx-0 dark:bg-gray-900 dark:text-white">
         <h2 className="text-2xl sm:text-xl mb-3 font-bold text-black dark:text-white">
           Get Started with Uber
