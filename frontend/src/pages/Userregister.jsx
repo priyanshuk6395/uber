c00624d98a90ef4ea8893176bc2f5e79dbefa7cf
Priyanshu Kumar <priyanshuk6395@gmail.com>
2025-03-04 22:59:25 +0530

    server check

--- frontend/src/pages/Userregister.jsx
+++ frontend/src/pages/Userregister.jsx
@@ -1,7 +1,7 @@
 import React, {useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import {UserDataContext} from "../context/userContext";
+import {UserDataContext} from "../context/UserContext";
 
 const UserRegister = () => {
   const [firstname, setFirstname] = useState('');
